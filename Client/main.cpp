6ed4afd59b34261602dbb454e7544fbb9947687d
Daniel Racz <daniel.racz@kuka.com>
2020-08-08 14:01:31 +0200

    Apply formatting to main.cpp files

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,43 +1,47 @@
 // Client side C/C++ program to demonstrate Socket programming
-#include <stdio.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define PORT 2222
 
-int main(int argc, char const *argv[]) {
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
+int main(int argc, char const* argv[])
+{
+   int sock = 0, valread;
+   struct sockaddr_in serv_addr;
+   char* hello = "Hello from client";
+   char buffer[1024] = {0};
+   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+   {
+      printf("\n Socket creation error \n");
+      return -1;
+   }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
+   // Convert IPv4 and IPv6 addresses from text to binary form
+   if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+   {
+      printf("\nInvalid address/ Address not supported \n");
+      return -1;
+   }
 
-    if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    int counter = 100;
-    while (counter-- > 0) {
-        send(sock, hello, strlen(hello), 0);
-        printf("Hello message sent %d/100 \n", counter
-        );
-        valread = read(sock, buffer, 1024);
-        printf("%s\n", buffer);
-        usleep(100);
-    }
-    return 0;
+   if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+   {
+      printf("\nConnection Failed \n");
+      return -1;
+   }
+   int counter = 100;
+   while (counter-- > 0)
+   {
+      send(sock, hello, strlen(hello), 0);
+      printf("Hello message sent %d/100 \n", counter);
+      valread = read(sock, buffer, 1024);
+      printf("%s\n", buffer);
+      usleep(100);
+   }
+   return 0;
 }
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,17 +1,13 @@
 #include <SocketCommunication/MultiClientTCPStreamListener.h>
 
-int main(int argc, char const *argv[]) {
-
-    MultiClientTCPStreamListener stream({
-                                                2222,
-                                                5
-                                        });
-    stream.init();
-    Logger logger("MAIN");
-    logger.info("Press [Enter] to exit");
-    getchar();
-    usleep(50000);
-    logger.info("Exiting main...");
-    return 0;
+int main(int argc, char const* argv[])
+{
+   MultiClientTCPStreamListener stream({2222, 5});
+   stream.init();
+   Logger logger("MAIN");
+   logger.info("Press [Enter] to exit");
+   getchar();
+   usleep(50000);
+   logger.info("Exiting main...");
+   return 0;
 }
-
